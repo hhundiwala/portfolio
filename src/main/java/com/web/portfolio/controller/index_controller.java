28d2c4ae034317df15e8bda396bf62b237ecc515
hhundiwala <hhundiwala@gmail.com>
2017-09-30 12:28:56 -0400

    Application created,application is working with initial content

---
+++ src/main/java/com/web/portfolio/controller/index_controller.java
@@ -0,0 +1,22 @@
+package com.web.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class index_controller {
+
+
+    @RequestMapping("/")
+    public String load_index() {
+        return "profile";
+    }
+
+    @RequestMapping("/more_info")
+    public String more_info() {
+        return "more_info";
+    }
+
+}
+
+
